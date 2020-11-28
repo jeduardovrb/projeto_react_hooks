ac41f217a3b2cacc85b323f507114481915eda93
Jorge Eduardo <jorgeeduardovrb@gmail.com>
2020-11-28 18:09:11 -0300

    ajustes finais

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,20 +2,18 @@ import React, {useState} from 'react';
 import './App.css';
 
 export default function App() {
-  
-  const [repositorio, setRepositorio] = useState ([{textini: 'Esse texto contém ', id: 0, textofim: ' caracteres.'}]);
+  const [txt, setTxt] = useState ([{textini: 'Esse texto contém ', id: 0, textofim: ' caracteres.'}]);
 
   const EditChannelName = (event) => {     
-     setRepositorio([{textini: 'Esse texto contém ', id: event.target.value.length, textofim: ' caracteres.'}])
-  }
+    setTxt([{textini: 'Esse texto contém ', id: event.target.value.length, textofim: ' caracteres.'}])
+  }   
 
   return (
     <div class="content">
       <form>
         <input type="text" onChange={EditChannelName}></input>
-        {repositorio.map(repo => (<p>{repo.textini} {repo.id} {repo.textofim}</p>))}
+          {txt.map(t => (<p>{t.textini} {t.id} {t.textofim}</p>))}
       </form>
     </div>
-  );
+  );  
 }
-
